da4aa31dd9c885d23758e55008572114587ab46e
Jade Thornton <jade@jmthornton.net>
2018-09-22 16:39:04 -0500

    build out nav

--- src/components/app/nav.jsx
+++ src/components/app/nav.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 export default class Nav extends React.PureComponent {
   render() {
     return (
-      <div>
-        <p>nav bar here</p>
-      </div>
+      <header className="sticky">
+        <a href="/" className="button" role="button">Home</a>
+        <a href="/#connect" className="button" role="button">Connect</a>
+        <a href="/gallery/" className="button" role="button">Gallery</a>
+        <a href="https://blog.jmthornton.net" className="button" role="button">Blog</a>
+      </header>
     );
   }
 }
