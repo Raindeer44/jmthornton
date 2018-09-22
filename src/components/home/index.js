979831d2af0e88734e166564554f774e0af1d100
Jade Thornton <jade@jmthornton.net>
2018-09-22 16:39:49 -0500

    sketch home page

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import About from './about';
+import Landing from './landing';
+import Projects from './projects';
+import Resume from './resume';
+
 /* eslint-disable react/prefer-stateless-function */
 export default class HomePage extends React.PureComponent {
   render() {
     return (
-      <h1>IT FUCKING WORKS</h1>
+      <div>
+        <Landing />
+        <About />
+        <Resume />
+        <Projects />
+      </div>
     );
   }
 }
