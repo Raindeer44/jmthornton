93d84b6c47fce23595e9a8f0dacb4643c3645a9a
Jade Thornton <jade@jmthornton.net>
2018-09-22 16:39:13 -0500

    build out footer

--- src/components/app/footer.jsx
+++ src/components/app/footer.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
+import * as pkg from '../../../package.json';
+const siteVersion = pkg.version;
+
 export default class Footer extends React.PureComponent {
   render() {
     return (
       <footer>
-        <p>This is foot</p>
+        <div className="row stats-row">
+          <div className="col-sm coffee-col">
+            <h2>99</h2><h4>cups coffee</h4>
+          </div>
+          <div className="col-sm project-col">
+            <h2>99</h2><h4>maintained projects</h4>
+          </div>
+          <div className="col-sm drone-col">
+            <iframe src='https://www.dronelogbook.com/widgets/stickerDashboard.php?token=37250' width='400' height='163' frameBorder='0' style={{overflow:'hidden'}} marginHeight='0' marginWidth='0'></iframe>
+          </div>
+        </div>
+        Jade Thornton | <a href="https://gitlab.com/thornjad/jmthornton">Source code</a> | <a href="/p/sitemap.xml">Sitemap</a> | <span className="version">{siteVersion}</span>
       </footer>
     );
   }
